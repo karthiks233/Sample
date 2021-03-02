b63f7c4101bc57ae067c54e5419e336f3e62d924
Karthik Raja <67165351+karthiks233@users.noreply.github.com>
2021-03-02 22:28:12 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -7,4 +7,4 @@ def test_index():
 
     r = client.get('/')
     assert r.status_code == 200
-    assert 'Hello World' in r.data.decode('utf-8')
+    assert 'Hello Karthik' in r.data.decode('utf-8')
